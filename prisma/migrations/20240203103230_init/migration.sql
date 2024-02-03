ba218b47b87edab7fe08a84113887f1d7196cb61
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-02-03 11:32:48 +0100

    Added createdAt and updatedAt to Todo model

--- prisma/migrations/20240203103230_init/migration.sql
+++ prisma/migrations/20240203103230_init/migration.sql
@@ -36,6 +36,8 @@ CREATE TABLE `Todo` (
     `content` VARCHAR(191) NOT NULL,
     `completed` BOOLEAN NOT NULL DEFAULT false,
     `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -8,7 +8,22 @@ export default async function TodosPage() {
 
     return (
         <main className="flex h-full w-full flex-col items-center justify-center gap-4">
-            test
+            <form className="flex flex-row items-center gap-4 text-lg font-bold">
+                <input
+                    type="text"
+                    name="todo"
+                    required
+                    id="todo"
+                    placeholder="Your todo"
+                    className="rounded-lg p-3 text-black"
+                />
+                <button
+                    type="submit"
+                    className="h-full rounded-lg bg-white px-6 py-2 capitalize text-black"
+                >
+                    Add
+                </button>
+            </form>
         </main>
     );
 }
