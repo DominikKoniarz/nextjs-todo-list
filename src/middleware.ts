a7b0d38af94889f182e43b3c6d393f3d40613acb
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-02-12 13:43:48 +0100

    Added csp and cors

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+    // Replace newline characters and spaces
+    const contentSecurityPolicyHeaderValue = cspHeader
+        .replace(/\s{2,}/g, " ")
+        .trim();
+
+    const response = NextResponse.next();
+
+    response.headers.set(
+        "Content-Security-Policy",
+        contentSecurityPolicyHeaderValue,
+    );
+
+    const allowedOrigin: string =
+        process.env.NODE_ENV === "development"
+            ? "*"
+            : process.env.NEXT_PUBLIC_APP_URL ?? "*";
+
+    // add the CORS headers to the response
+    response.headers.append("Access-Control-Allow-Credentials", "true");
+    response.headers.append("Access-Control-Allow-Origin", allowedOrigin);
+    response.headers.append(
+        "Access-Control-Allow-Methods",
+        "GET,DELETE,PATCH,POST,PUT",
+    );
+    response.headers.append(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-Type, Date",
+    );
+
+    return response;
+}
